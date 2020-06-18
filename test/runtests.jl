72733b580fb036bcb48a3338e5efd3deeb95470f
Jutho Haegeman <jutho.haegeman@ugent.be>
2020-06-18 17:26:08 +0200

    try to make tests more robust

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using CMPSKit
 using Test
 using LinearAlgebra
+using Random
+Random.seed!(1111)
 
 include("fourierseries.jl")
 include("uniformcmps.jl")
--- test/uniformcmps.jl
+++ test/uniformcmps.jl
@@ -1,16 +1,17 @@
 Dlist = (2,4,12,29,71)
 
+
 @testset "UniformCMPS: environments and gauging with bond dimension $D" for D in Dlist
     for T in (Float64, ComplexF64)
         Q = Constant(randn(T, (D,D)))
         R1 = Constant(randn(T, (D,D)))
         R2 = Constant(randn(T, (D,D)))
         Ψ = InfiniteCMPS(Q, (R1,R2))
-        ρL, λL, infoL = leftenv(Ψ; krylovdim = min(D^2,50))
+        ρL, λL, infoL = leftenv(Ψ; krylovdim = min(D^2,50), tol = 1e-12)
         @test norm(Q'*ρL + ρL*Q + R1'*ρL*R1 + R2'*ρL*R2 - 2*λL*ρL) <= 10*infoL.normres[1]
         @test ρL == ρL'
 
-        ρR, λR, infoR = rightenv(Ψ; krylovdim = min(D^2,50))
+        ρR, λR, infoR = rightenv(Ψ; krylovdim = min(D^2,50), tol = 1e-12)
         @test norm(Q*ρR + ρR*Q' + R1*ρR*R1' + R2*ρR*R2' - 2*λR*ρR) <= 10*infoR.normres[1]
         @test λL ≈ λR
         @test ρR == ρR'
@@ -19,15 +20,15 @@ Dlist = (2,4,12,29,71)
         @test imag(Z) <= sqrt(infoL.normres[1]*infoR.normres[1])
 
         Ψn = copy(Ψ)
-        ρL2, ρR2, = environments!(Ψn; krylovdim = min(D^2,50))
+        ρL2, ρR2, = environments!(Ψn; krylovdim = min(D^2,50), tol = 1e-12)
         @test ρL2 ≈ ρL/sqrt(Z)
         @test ρR2 ≈ ρR/sqrt(Z)
         Qn = Ψn.Q
         R1n, R2n = Ψn.Rs
-        @test norm(Qn'*ρL + ρL*Qn + R1n'*ρL*R1n + R2n'*ρL*R2n) <= 1e-10
-        @test norm(Qn*ρR + ρR*Qn' + R1n*ρR*R1n' + R2n*ρR*R2n') <= 1e-10
+        @test norm(Qn'*ρL + ρL*Qn + R1n'*ρL*R1n + R2n'*ρL*R2n) <= 1e-9
+        @test norm(Qn*ρR + ρR*Qn' + R1n*ρR*R1n' + R2n*ρR*R2n') <= 1e-9
 
-        ΨL,λ,CL,info = leftgauge(Ψ; tol = 1e-13)
+        ΨL,λ,CL,info = leftgauge(Ψ; tol = 1e-12)
         @test λ ≈ λL
         @test CL'*CL ≈ ρL/tr(ρL[])
         QL = ΨL.Q
@@ -35,9 +36,9 @@ Dlist = (2,4,12,29,71)
         @test CL*Qn ≈ QL*CL
         @test CL*R1 ≈ R1L*CL
         @test CL*R2 ≈ R2L*CL
-        @test norm(QL + QL' + R1L'*R1L + R2L'*R2L) <= 1e-10
+        @test norm(QL + QL' + R1L'*R1L + R2L'*R2L) <= 1e-9
 
-        ΨR,λ,CR,info = rightgauge(Ψ; tol = 1e-13)
+        ΨR,λ,CR,info = rightgauge(Ψ; tol = 1e-12)
         @test λ ≈ λR
         @test CR*CR' ≈ ρR/tr(ρR[])
         QR = ΨR.Q
@@ -45,6 +46,6 @@ Dlist = (2,4,12,29,71)
         @test Qn*CR ≈ CR*QR
         @test R1*CR ≈ CR*R1R
         @test R2*CR ≈ CR*R2R
-        @test norm(QR + QR' + R1R*R1R' + R2R*R2R') <= 1e-10
+        @test norm(QR + QR' + R1R*R1R' + R2R*R2R') <= 1e-9
     end
 end
